5c2ea093698b76a68c385d2c1093d5906b43216b
YashwanthReddy <yash868686reddy@gmail.com>
2021-10-10 10:27:41 -0500

    Added Header and Meals

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,8 +1,15 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
 function App() {
   return (
-    <div className="App">
-      Getting Started
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals/>
+      </main>
+    </Fragment>
   );
 }
 
